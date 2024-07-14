2118c9eea8fbeeac430a135892f24f4ade1c9371
ProudOne7 <peternyen7@gmail.com>
2024-07-13 22:36:20 -0500

    Added Animation to Buttons

--- Client/src/Components/dashboard/dashboard.tsx
+++ Client/src/Components/dashboard/dashboard.tsx
@@ -1,4 +1,8 @@
-import { Box, Center, Flex, Grid, GridItem } from "@chakra-ui/react";
+import { Box, Center, Flex, Grid, GridItem, Text } from "@chakra-ui/react";
+import { ViewIcon, TimeIcon } from '@chakra-ui/icons'
+import { FaFile } from "react-icons/fa";
+
+import { motion } from "framer-motion";
 
 function dashboard() {
   return (
@@ -6,25 +10,36 @@ function dashboard() {
         <Grid w={'90%'} h={'90%'} templateRows={'repeat(2, 1fr)'} templateColumns={'repeat(2, 1fr)'} gap={4} p={'1rem'} border={'.5rem solid white'} borderRadius={'1rem'}>
             <GridItem>
                 <Flex w={'100%'} h={'100%'} justify={'center'} align={'center'}>
-                    <Box w={'125px'} h={'125px'} bg={'white'} borderRadius={'1rem'}></Box>
+                    <Flex as={motion.div} w={'125px'} h={'125px'} direction={'column'} justify={'center'} align={'center'} bg={'white'} borderRadius={'1rem'} whileHover={{scale: 1.05}} whileTap={{scale: 0.95}}>
+                        <ViewIcon boxSize={6} />
+                        <Text fontFamily={'Fonarto'} textAlign={'center'}>View PDF</Text>
+                    </Flex>
                 </Flex>
             </GridItem>
 
             <GridItem>
                 <Flex w={'100%'} h={'100%'} justify={'center'} align={'center'}>
-                    <Box w={'125px'} h={'125px'} bg={'white'} borderRadius={'1rem'}></Box>
+                    <Flex as={motion.div} w={'125px'} h={'125px'} direction={'column'} justify={'center'} align={'center'} bg={'white'} borderRadius={'1rem'} whileHover={{scale: 1.05}} whileTap={{scale: 0.95}}>
+                        <FaFile size={'1.5rem'}/>
+                        <Text fontFamily={'Fonarto'} textAlign={'center'} mx={'1rem'}>Convert FIle To PDF</Text>
+                    </Flex>
                 </Flex>
             </GridItem>
 
             <GridItem>
                 <Flex w={'100%'} h={'100%'} justify={'center'} align={'center'}>
-                    <Box w={'125px'} h={'125px'} bg={'white'} borderRadius={'1rem'}></Box>
+                    <Flex as={motion.div} w={'125px'} h={'125px'} direction={'column'} justify={'center'} align={'center'} bg={'white'} borderRadius={'1rem'} whileHover={{scale: 1.05}} whileTap={{scale: 0.95}}>
+
+                    </Flex>
                 </Flex>
             </GridItem>
 
             <GridItem>
                 <Flex w={'100%'} h={'100%'} justify={'center'} align={'center'}>
-                    <Box w={'125px'} h={'125px'} bg={'white'} borderRadius={'1rem'}></Box>
+                    <Flex as={motion.div} w={'125px'} h={'125px'} direction={'column'} justify={'center'} align={'center'} bg={'white'} borderRadius={'1rem'} whileHover={{scale: 1.05}} whileTap={{scale: 0.95}}>
+                        <TimeIcon boxSize={6} />
+                        <Text fontFamily={'Fonarto'} textAlign={'center'}>Timer</Text>
+                    </Flex>
                 </Flex>
             </GridItem>
         </Grid>
